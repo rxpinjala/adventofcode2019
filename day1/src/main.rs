c4aa5d949ae5d03810823b07cad1198a6a3156a9
Ravi Pinjala <ravixpinjala@gmail.com>
2020-04-15 21:42:44 -0700

    Solution to Day 1, part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,11 +2,13 @@ use std::env;
 use std::fs;
 use std::io;
 
+static PART2: bool = true;
+
 fn main() {
     let input: &String = &env::args().nth(1).unwrap();
     
     let input_data = read_input(&input).unwrap();
-    let result = calculate_fuel(input_data);
+    let result = calc_fuel(&input_data);
 
     println!("Total fuel: {}", result);
 }
@@ -17,12 +19,30 @@ fn read_input(path: &str) -> io::Result<Vec<i32>> {
     Ok(numbers)
 }
 
-fn calculate_fuel(input_data: Vec<i32>) -> i32 {
+fn calc_module_fuel(input: i32) -> i32 {
+    (input / 3) - 2
+}
+
+fn calc_fuel(input_data: &Vec<i32>) -> i32 {
     let mut total_fuel: i32 = 0;
 
     for n in input_data.iter() {
-        let fuel = (n / 3) - 2;
+        let fuel = calc_module_fuel(*n);
         total_fuel = total_fuel + fuel;
+        if PART2 {
+            total_fuel = total_fuel + calc_additional_fuel(fuel);
+        }
+    }
+
+    total_fuel
+}
+
+fn calc_additional_fuel(input: i32) -> i32 {
+    let mut total_fuel = 0;
+    let mut additional_fuel = calc_module_fuel(input);
+    while additional_fuel > 0 {
+        total_fuel = total_fuel + additional_fuel;
+        additional_fuel = calc_module_fuel(additional_fuel);
     }
 
     total_fuel
