8262e8bee502d8c850412e76160c38b46b232c5a
Ravi Pinjala <ravixpinjala@gmail.com>
2020-04-30 22:49:25 -0700

    Day 5, part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,8 +2,13 @@ use std::env;
 
 fn main() {
     let input_path: &String = &env::args().nth(1).unwrap();
-    let mut p = intcode::read_from_path(input_path).unwrap();
+    let p = intcode::read_from_path(input_path).unwrap();
 
-    let output = intcode::run(&mut p, 0, vec!(1)).unwrap();
+    let mut p1 = p.clone();
+    let output = intcode::run(&mut p1, 0, vec!(1)).unwrap();
+    dbg!(output);
+
+    let mut p2 = p.clone();
+    let output = intcode::run(&mut p2, 0, vec!(5)).unwrap();
     dbg!(output);
 }
