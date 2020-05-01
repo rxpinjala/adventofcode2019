e96468675f648b4b48d669bc63fbda4ca6005d86
Ravi Pinjala <ravixpinjala@gmail.com>
2020-04-30 22:35:19 -0700

    Implement new opcodes for day 5 part 2

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -16,6 +16,10 @@ pub enum Inst {
     Mult(InParam, InParam, OutParam),
     Input(OutParam),
     Output(InParam),
+    JumpIfTrue(InParam, InParam),
+    JumpIfFalse(InParam, InParam),
+    LessThan(InParam, InParam, OutParam),
+    Equal(InParam, InParam, OutParam),
     Exit,
 }
 
@@ -27,6 +31,10 @@ impl Inst {
             Inst::Exit => 1,
             Inst::Input(_) => 2,
             Inst::Output(_) => 2,
+            Inst::JumpIfTrue(_,_) => 3,
+            Inst::JumpIfFalse(_,_) => 3,
+            Inst::LessThan(_,_,_) => 4,
+            Inst::Equal(_,_,_) => 4,
         }
     }
 }
@@ -78,6 +86,50 @@ pub fn decode(p: &[i32], pc: usize) -> Result<Inst, &'static str> {
             }
         }
 
+        5 => { // jump if true
+            let inst = p.get(pc..pc + 3).ok_or("Bad jump-if-true instruction")?;
+            match modes {
+                0 => Ok(Inst::JumpIfTrue(InParam::Position(inst[1]), InParam::Position(inst[2]))),
+                1 => Ok(Inst::JumpIfTrue(InParam::Immediate(inst[1]), InParam::Position(inst[2]))),
+                10 => Ok(Inst::JumpIfTrue(InParam::Position(inst[1]), InParam::Immediate(inst[2]))),
+                11 => Ok(Inst::JumpIfTrue(InParam::Immediate(inst[1]), InParam::Immediate(inst[2]))),
+                _ => Err("Bad jump-if-true instruction modes")
+            }
+        }
+
+        6 => { // jump if false
+            let inst = p.get(pc..pc + 3).ok_or("Bad jump-if-false instruction")?;
+            match modes {
+                0 => Ok(Inst::JumpIfFalse(InParam::Position(inst[1]), InParam::Position(inst[2]))),
+                1 => Ok(Inst::JumpIfFalse(InParam::Immediate(inst[1]), InParam::Position(inst[2]))),
+                10 => Ok(Inst::JumpIfFalse(InParam::Position(inst[1]), InParam::Immediate(inst[2]))),
+                11 => Ok(Inst::JumpIfFalse(InParam::Immediate(inst[1]), InParam::Immediate(inst[2]))),
+                _ => Err("Bad jump-if-false instruction modes")
+            }
+        }
+
+        7 => { // less than
+            let inst = p.get(pc..pc + 4).ok_or("Bad less-than instruction")?;
+            match modes {
+                0 => Ok(Inst::LessThan(InParam::Position(inst[1]), InParam::Position(inst[2]), OutParam::Position(inst[3]))),
+                1 => Ok(Inst::LessThan(InParam::Immediate(inst[1]), InParam::Position(inst[2]), OutParam::Position(inst[3]))),
+                10 => Ok(Inst::LessThan(InParam::Position(inst[1]), InParam::Immediate(inst[2]), OutParam::Position(inst[3]))),
+                11 => Ok(Inst::LessThan(InParam::Immediate(inst[1]), InParam::Immediate(inst[2]), OutParam::Position(inst[3]))),
+                _ => Err("Bad less-than instruction modes")
+            }
+        }
+
+        8 => { // equal
+            let inst = p.get(pc..pc + 4).ok_or("Bad equal instruction")?;
+            match modes {
+                0 => Ok(Inst::Equal(InParam::Position(inst[1]), InParam::Position(inst[2]), OutParam::Position(inst[3]))),
+                1 => Ok(Inst::Equal(InParam::Immediate(inst[1]), InParam::Position(inst[2]), OutParam::Position(inst[3]))),
+                10 => Ok(Inst::Equal(InParam::Position(inst[1]), InParam::Immediate(inst[2]), OutParam::Position(inst[3]))),
+                11 => Ok(Inst::Equal(InParam::Immediate(inst[1]), InParam::Immediate(inst[2]), OutParam::Position(inst[3]))),
+                _ => Err("Bad equal instruction modes")
+            }
+        }
+
         99 => Ok(Inst::Exit),
 
         _ => Err("Bad instruction")
@@ -123,6 +175,54 @@ fn test_decode_output() {
     assert_eq!(inst, Inst::Output(InParam::Immediate(0)));
 }
 
+#[test]
+fn test_decode_jumpiftrue() {
+    let inst = decode(&vec!(5,1,2)[..], 0).unwrap();
+    assert_eq!(inst, Inst::JumpIfTrue(InParam::Position(1), InParam::Position(2)));
+
+    let inst = decode(&vec!(105,1,2)[..], 0).unwrap();
+    assert_eq!(inst, Inst::JumpIfTrue(InParam::Immediate(1), InParam::Position(2)));
+
+    let inst = decode(&vec!(1105,1,2)[..], 0).unwrap();
+    assert_eq!(inst, Inst::JumpIfTrue(InParam::Immediate(1), InParam::Immediate(2)));
+}
+
+#[test]
+fn test_decode_jumpiffalse() {
+    let inst = decode(&vec!(6,1,2)[..], 0).unwrap();
+    assert_eq!(inst, Inst::JumpIfFalse(InParam::Position(1), InParam::Position(2)));
+
+    let inst = decode(&vec!(106,1,2)[..], 0).unwrap();
+    assert_eq!(inst, Inst::JumpIfFalse(InParam::Immediate(1), InParam::Position(2)));
+
+    let inst = decode(&vec!(1106,1,2)[..], 0).unwrap();
+    assert_eq!(inst, Inst::JumpIfFalse(InParam::Immediate(1), InParam::Immediate(2)));
+}
+
+#[test]
+fn test_decode_lessthan() {
+    let inst = decode(&vec!(7,1,2,3)[..], 0).unwrap();
+    assert_eq!(inst, Inst::LessThan(InParam::Position(1), InParam::Position(2), OutParam::Position(3)));
+
+    let inst = decode(&vec!(107,1,2,3)[..], 0).unwrap();
+    assert_eq!(inst, Inst::LessThan(InParam::Immediate(1), InParam::Position(2), OutParam::Position(3)));
+
+    let inst = decode(&vec!(1107,1,2,3)[..], 0).unwrap();
+    assert_eq!(inst, Inst::LessThan(InParam::Immediate(1), InParam::Immediate(2), OutParam::Position(3)));
+}
+
+#[test]
+fn test_decode_equal() {
+    let inst = decode(&vec!(8,1,2,3)[..], 0).unwrap();
+    assert_eq!(inst, Inst::Equal(InParam::Position(1), InParam::Position(2), OutParam::Position(3)));
+
+    let inst = decode(&vec!(108,1,2,3)[..], 0).unwrap();
+    assert_eq!(inst, Inst::Equal(InParam::Immediate(1), InParam::Position(2), OutParam::Position(3)));
+
+    let inst = decode(&vec!(1108,1,2,3)[..], 0).unwrap();
+    assert_eq!(inst, Inst::Equal(InParam::Immediate(1), InParam::Immediate(2), OutParam::Position(3)));
+}
+
 pub fn load(p: &[i32], param: &InParam) -> Result<i32, &'static str> {
     match param {
         InParam::Immediate(i) => Ok(*i),
@@ -154,6 +254,7 @@ pub fn run(p: &mut Vec<i32>, start: usize, input: Vec<i32>) -> Result<Vec<i32>, 
 
     loop {
         let inst = decode(&p[..], pc)?;
+        pc = pc + inst.len();
         match &inst {
             Inst::Add(src1, src2, dst) => {
                 let p1 = load(&p[..], &src1)?;
@@ -171,15 +272,48 @@ pub fn run(p: &mut Vec<i32>, start: usize, input: Vec<i32>) -> Result<Vec<i32>, 
                 let value = input.remove(0);
                 store(&mut p[..], &dst, value)?;
                 Ok(())
-            }
+            },
             Inst::Output(src) => {
                 let p1 = load(&p[..], &src)?;
                 output.push(p1);
                 Ok(())
+            },
+            Inst::JumpIfTrue(cond, target) => {
+                let cond = load(&p[..], &cond)?;
+                if cond != 0 {
+                    pc = load(&p[..], &target)? as usize;
+                }
+                Ok(())
+            },
+            Inst::JumpIfFalse(cond, target) => {
+                let cond = load(&p[..], &cond)?;
+                if cond == 0 {
+                    pc = load(&p[..], &target)? as usize;
+                }
+                Ok(())
+            },
+            Inst::LessThan(src1, src2, dst) => {
+                let p1 = load(&p[..], &src1)?;
+                let p2 = load(&p[..], &src2)?;
+                if p1 < p2 {
+                    store(&mut p[..], &dst, 1)?;
+                } else {
+                    store(&mut p[..], &dst, 0)?;
+                }
+                Ok(())
+            }
+            Inst::Equal(src1, src2, dst) => {
+                let p1 = load(&p[..], &src1)?;
+                let p2 = load(&p[..], &src2)?;
+                if p1 == p2 {
+                    store(&mut p[..], &dst, 1)?;
+                } else {
+                    store(&mut p[..], &dst, 0)?;
+                }
+                Ok(())
             }
             Inst::Exit => break
         }?;
-        pc = pc + inst.len();
     }
 
     Ok(output)
@@ -222,6 +356,17 @@ fn run_with_io() {
     let result = run(&mut p, 0, vec!(0)).unwrap();
     assert_eq!(p, vec!(3,5,104,10,4,0,99));
     assert_eq!(result, vec!(10,3));
+}
+
+#[test]
+fn run_with_comparisons() {
+    let mut p = vec!(3,9,8,9,10,9,4,9,99,-1,8);
+    let result = run(&mut p, 0, vec!(7)).unwrap();
+    assert_eq!(result, vec!(0));
+
+    let mut p = vec!(3,9,8,9,10,9,4,9,99,-1,8);
+    let result = run(&mut p, 0, vec!(8)).unwrap();
+    assert_eq!(result, vec!(1));
 }
 
 }
