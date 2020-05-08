3a8ee2fcc9ad61815e4688aed4f9e131362c96e9
Ravi Pinjala <ravixpinjala@gmail.com>
2020-05-07 18:36:17 -0700

    Day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+fn main() {
+    let input_path: &String = &env::args().nth(1).unwrap();
+
+    let mut c = intcode::Computer::load_from_path(input_path).unwrap();
+    c.input.push_back(1);
+    c.run();
+    println!("Part 1: {}", c.take_output()[0]);
+
+    let mut c = intcode::Computer::load_from_path(input_path).unwrap();
+    c.input.push_back(2);
+    c.run();
+    println!("Part 2: {}", c.take_output()[0]);
+}
